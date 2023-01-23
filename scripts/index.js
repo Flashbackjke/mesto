3944e97b2d835de21f33369d8a9796213fdff63a
Evgeniy Eroshkin <flashback@yandex.ru>
2023-01-23 23:27:47 +0700

    исправления ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 let popup = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__input_name');
-let infoInput = document.querySelector('.popup__input_info');
+let nameInput = document.querySelector('.popup__container-input_name');
+let infoInput = document.querySelector('.popup__container-input_info');
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
 let edit = document.querySelector('.profile__edit');
 let close = document.querySelector('.popup__close');
-let save = document.querySelector('.popup__container');
+let save = document.querySelector('.popup__form');
 
-edit.addEventListener('click',OpenEdit);
-close.addEventListener('click',CloseEdit);
-save.addEventListener('submit',SaveEdit);
+edit.addEventListener('click',openEdit);
+close.addEventListener('click',closeEdit);
+save.addEventListener('submit',saveEdit);
 
-function OpenEdit() {
-  popup.classList.toggle('popup_opened');
+function openEdit() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   infoInput.value = profileAbout.textContent;
 }
 
-function CloseEdit() {
-  popup.classList.toggle('popup_opened');
+function closeEdit() {
+  popup.classList.remove('popup_opened');
 }
 
-function SaveEdit(event) {
+function saveEdit(event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = infoInput.value;
-  popup.classList.toggle('popup_opened');
+  closeEdit();
 }
